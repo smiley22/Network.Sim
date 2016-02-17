8e89ede4d83a33e47a7ef9e684f1757e72a9e46e
Torben Könke <torben.koenke@gmail.com>
2016-02-17 12:42:39 +0100

    Refactoring and adding xml doc documentation.

--- Cable/C10Base2.cs
+++ Cable/C10Base2.cs
@@ -1,25 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Network.Sim.Core;
 
 namespace Network.Sim.Core {
+    /// <summary>
+    /// Represents a cable as is used in 10Base2 Ethernet installations.
+    /// </summary>
 	public class C10Base2 : Cable {
-		// 10BASE2 runs at a speed of 10Mbps.
-		static readonly int maxBitrate = 10000000;
-		// 10BASE2 uses RG-58A/U coaxial cables. 
-		static readonly double velocityFactor = 0.66;
+        /// <summary>
+        /// The maximum number of transferable bits per second.
+        /// </summary>
+        /// <remarks>
+        ///  10BASE2 runs at a speed of 10Mbps.
+        /// </remarks>
+        const int maxBitrate = 10000000;
 
-		public C10Base2(int length)
-			: base(length, maxBitrate, velocityFactor, false) {
+        /// <summary>
+        /// The speed at which a signal propagates through the cable, expressed as a fraction of
+        /// the speed of light.
+        /// </summary>
+        /// <remarks>
+        /// 10BASE2 uses RG-58A/U coaxial cables.
+        /// </remarks>
+        const double velocityFactor = 0.66;
+
+        /// <summary>
+        /// Initializes a new instance of the C10Base2 class.
+        /// </summary>
+        /// <param name="length">
+        /// The length of the cable, in metres.
+        /// </param>
+        public C10Base2(int length) : base(length, maxBitrate, velocityFactor, false) {
 		}
 
+        /// <summary>
+        /// Attach the specified connector to the cable at the specified position.
+        /// </summary>
+        /// <param name="position">The position at which to connect the connector to the
+        /// cable.</param>
+        /// <param name="connector">The connector to connect to the cable at the specified
+        /// position.</param>
+        /// <exception cref="InvalidOperationException">Another connector has already been
+        /// installed at the specified position.</exception>
+        /// <exception cref="InvalidOperationException">As per 10BASE2 specification the
+        /// position at which a station is connected must be a multiple of 0.5 metres.
+        /// </exception>
 		public void Attach(double position, Connector connector) {
 			if (connectors.Values.Contains(position))
 				throw new InvalidOperationException("The position is already taken.");
-			if ((position % 0.5) != 0.0)
+			if (Math.Abs(position % 0.5) > .0)
 				throw new InvalidOperationException("Position must be a multiple of 0.5.");
 			if (connectors.ContainsKey(connector))
 				throw new InvalidOperationException("Connector is already attached at " +
--- Cable/C10Base5.cs
+++ Cable/C10Base5.cs
@@ -1,30 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Network.Sim.Core;
 
 namespace Network.Sim.Core {
+    /// <summary>
+    /// Represents a cable as is used in 10Base5 Ethernet installations.
+    /// </summary>
 	public class C10Base5 : Cable {
+        /// <summary>
+        /// The maximum number of transferable bits per second.
+        /// </summary>
+	    const int maxBitrate = 10000000;
 
-		static readonly int maxBitrate = 10000000;
-		static readonly double velocityFactor = 0.66;
+        /// <summary>
+        /// The speed at which a signal propagates through the cable, expressed as a fraction of
+        /// the speed of light.
+        /// </summary>
+	    const double velocityFactor = 0.66;
 
-		public C10Base5(int length)
-			: base(length, maxBitrate, velocityFactor, false) {
-		}
+        /// <summary>
+        /// Initializes a new instance of the C10Base5 class.
+        /// </summary>
+        /// <param name="length">
+        /// The length of the cable, in metres.
+        /// </param>
+        public C10Base5(int length) : base(length, maxBitrate, velocityFactor, false) {
+        }
 
+        /// <summary>
+        /// Pierces the cable and wires the specified connector to at the specified position.
+        /// </summary>
+        /// <param name="position">The position at which to pierce the cable.</param>
+        /// <param name="connector">The connector to wire to the cable at the pierced
+        /// position.</param>
+        /// <returns>A reference to the C10Base5 instance for chaining.</returns>
+        /// <exception cref="InvalidOperationException">Another connector has already been
+        /// installed at the specified position.</exception>
+        /// <exception cref="InvalidOperationException">As per IEEE 802.3 specification the
+        /// position at which the cable is pierced must be a multiple of 2.5 metres.</exception>
 		public C10Base5 Pierce(double position, Connector connector) {
 			if (connectors.Values.Contains(position))
 				throw new InvalidOperationException("The position is already taken.");
-			if ((position % 2.5) != 0.0)
+			if (Math.Abs(position % 2.5) > 0.1)
 				throw new InvalidOperationException("Position must be a multiple of 2.5.");
 			connectors.Add(connector, position);
 			connector.Cable = this;
-
 			return this;
 		}
-
 	}
 }
